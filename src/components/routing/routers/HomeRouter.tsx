978d1e3a8d4e4ecf6bc5b256bef3dd081dc7eaf7
MartinFaeh <121894103+Ristafan@users.noreply.github.com>
2024-12-30 22:50:38 +0100

    404 changes

--- src/components/routing/routers/HomeRouter.tsx
+++ src/components/routing/routers/HomeRouter.tsx
@@ -1,11 +1,12 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import NotFound from "../../views/NotFound"
+import Home from "../../views/Home.tsx";
 
 const HomeRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/home" element={<HomeRouter/>} />
+                <Route path="/home" element={<Home/>} />
                 <Route path="/*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
